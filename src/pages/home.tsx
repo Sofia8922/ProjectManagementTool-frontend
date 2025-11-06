5a808770e35f283c65458d18d3fbec77c5966faa
Siem Nijskens <siemnijskens@gmail.com>
2025-11-06 15:47:04 +0100

    cleaned up the pages of our app by inserting the modals through the components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,13 @@
-import { Button } from "react-bootstrap";
-import CustomModal from "../Components/CustomModal";
-import { useState } from "react";
-import { Form } from "react-bootstrap";
+import CreateNewAccountModal from "../Components/CreateNewAccountModal";
 
 const Homepage = () => {
-
-    const [showNewAccountModal, setShowNewAccountModal] = useState(false);
-    const handleSubmit = () => {
-        console.log("handled submit")
-    };
 
     return (
         <>
             <p>
                 Homepage
                 <br></br>
-                <Button variant="primary" value={"Create new account"} onClick={() => setShowNewAccountModal(true)} />
-
-                <CustomModal title="hoi ik ben een modal" handleSubmit={handleSubmit} show={showNewAccountModal} setShow={setShowNewAccountModal} >
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                    </Form>
-                </CustomModal>
+                <CreateNewAccountModal/>
             </p>
         </>
     )
--- src/pages/projectDetail.tsx
+++ src/pages/projectDetail.tsx
@@ -1,8 +1,21 @@
+import EditProjectModal from "../Components/EditProjectModal";
+import ManageLabelsModal from "../Components/ManageLabelsModal";
+import NewTaskModal from "../Components/NewTaskModal";
+
 const ProjectDetail = () => {
+
     return (
-        <h1>
-            Project Detail page
-        </h1>
+        <p>
+            <h1>Project Detail page</h1>
+            <br></br>
+
+            <EditProjectModal/>
+            <NewTaskModal/>
+            <ManageLabelsModal/>
+
+
+
+        </p>
     )
 }
 
