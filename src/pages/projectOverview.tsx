08291db595552896928b4080a25c6376564655ca
Arne <arneemil@xs4all.nl>
2025-11-11 14:05:05 +0100

    Added project list in projectOverview

--- src/pages/projectOverview.tsx
+++ src/pages/projectOverview.tsx
@@ -1,24 +1,62 @@
 // import account store
 
+import { useQuery } from "@tanstack/react-query";
 import NewProjectModal from "../Components/NewProjectModal";
+import { Fragment } from "react/jsx-runtime";
 
 const ProjectOverview = () => {
-    
+
+    const {
+        data: account,
+        isLoading,
+        error
+    } = useQuery({
+        queryKey: ["account"],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:8080/accounts/1`);
+            if (!response.ok) {
+                throw new Error("account error")
+            }
+            return response.json();
+        },
+    })
+
+    if (isLoading) {
+        return <p>loading</p>
+    }
+    if (error) {
+        return <p>error</p>
+    }
+
+    console.log(account)
+
     return (
         <>
             <div>
-                {/* als de rol van ingelogd account OWNER is dan wordt knop geshowed */}
-                {/* {Account.role === OWNER && <button>new project</button>} */}
-                <NewProjectModal/>
+                {account.role === "OWNER" && <NewProjectModal />}
+                {console.log(account.madeProjects.id + " hallo")}
                 <h1>
                     Project Overview page
                 </h1>
-                <h4>logged in as:</h4> {/* {Account.name} */}
+                <h4>logged in as:</h4> {account.name}
                 {/* set account store to null */}
                 <button>logout</button>
             </div>
             <div>
                 <h4>Ongoing projects</h4>
+                
+                {account.madeProjects && account.madeProjects.length > 0 ? (
+                    <>
+                        {account.madeProjects.map((madeProject) =>
+                            <Fragment key={madeProject.id}>
+                                <li>
+                                    <h5>{madeProject.name}</h5>
+                                    <p>{madeProject.description}</p>
+                                </li>
+                            </Fragment>
+                        )}
+                    </>) : (<>No projects found</>)}
+
                 {/* {map Account.projects if status==ongoing} + onclick setProjectId*/}
                 {/* acount.projects.name + account.projects.description account.projects.progress */}
             </div>
