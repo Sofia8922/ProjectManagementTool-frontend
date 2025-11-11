73596e9b2c2d5f288f27193b79f145f78cdad9af
Arne <arneemil@xs4all.nl>
2025-11-11 15:44:45 +0100

    added types folder and filled it with types

---
+++ src/types/Task.ts
@@ -0,0 +1,36 @@
+import type { AccountShortDTO } from "./Account";
+import type { CommentShortDTO } from "./Comment";
+import type { ProjectShortDTO } from "./Project";
+import type { TagDTO } from "./Tag";
+
+export interface TaskCreateDTO {
+  name: string;
+  content: string;
+  creatorId: number;
+  projectId: number;
+}
+
+export interface TaskDTO {
+  id: number;
+  name: string;
+  content: string;
+  status: string;
+  project: ProjectShortDTO;
+  comments: CommentShortDTO[];
+  creator: AccountShortDTO;
+  tags: TagDTO[];
+  assignedDevelopers: AccountShortDTO[];
+}
+
+export interface TaskEditDTO {
+  name: string;
+  content: string;
+  status: string;
+}
+
+export interface TaskShortDTO {
+    id: number;
+    name: string;
+    content: string;
+    status: string;
+}
