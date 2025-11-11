8790347a56f40468bfb6665e751f438b37943025
Patrick Boot <raventus@hotmail.nl>
2025-11-11 11:08:20 +0100

    Added UseStore to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import Router from './router';
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { createStore } from "@odemian/react-store";
+
+// De momentele gebruiker, lekker hoog in App zodat ie overal berijkbaar is
+export const [useUser, updateUser] = createStore({
+  name: "",
+  id: "",
+});
 
 function App() {
   const queryClient = new QueryClient();
