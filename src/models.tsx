dd399cf1f2bbf4994d2b6355d47d688e00c0fa56
Patrick Boot <raventus@hotmail.nl>
2025-11-12 14:51:20 +0100

    Added AccountCreationDTO to models

--- src/models.tsx
+++ src/models.tsx
@@ -2,3 +2,12 @@ export interface LoginDTO {
     email:string,
     password:string
 }
+
+export interface AccountCreationDTO {
+    name:string,
+    email:string,
+    role:RoleEnums,
+    password:string    
+}
+
+export type RoleEnums = 'Owner' | 'Developer' | 'Customer'
