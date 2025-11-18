338507142acb369a4fb0ded297606b7657be0c59
Siem Nijskens <siemnijskens@gmail.com>
2025-11-18 11:32:04 +0100

    added all horizontal scroll bars and cleaned up code

--- src/pages/projectOverview.tsx
+++ src/pages/projectOverview.tsx
@@ -2,18 +2,12 @@
 import { Card, Col } from "react-bootstrap";
 import { useQuery } from "@tanstack/react-query";
 import NewProjectModal from "../Components/NewProjectModal";
-// import { Fragment } from "react/jsx-runtime";
-import type { ProjectShortDTO } from "../types/Project";
 import { useState } from "react";
 import { API_URL } from "../App";
 import { useUser } from "../stores/userStore";
-// import InfiniteScroll from "../Components/InfiniteHorizontalScroll";
-import ScrollLinked from "../Components/HorizontalScrollBar";
-// import type { ProjectShortDTO } from "../Types/Project";
-// import ProjectOverviewComponent from '../Components/ProjectOverviewComponent';
+import ScrollLinkedProjects from "../Components/HorizontalScrollBarProjects";
 
 const ProjectOverview = () => {
-
 
     const [projectId, setProjectId] = useState<number>(NaN);
     const user = useUser();
@@ -42,19 +36,6 @@ const ProjectOverview = () => {
 
     console.log(account.madeProjects)
 
-    // console.log(typeof( account.madeProjects ))
-
-    // const accountArray = Object.entries(account.madeProjects)
-
-    // console.log(typeof(accountArray))
-    // const accountsArray2 = Object.entries(accountArray)
-
-    // console.log(typeof(accountsArray2))
-    //     let data: any = [1, 2, 3];
-    // let numbers: number[] = data as number[];
-
-    // const accountsArray : ProjectShortDTO[] = account.madeProjects as ProjectShortDTO[]
-
     return (
         <>
             <Card>
@@ -78,14 +59,19 @@ const ProjectOverview = () => {
                     <h4>Ongoing projects</h4>
                     {account.madeProjects && account.madeProjects.length > 0 ? (
                         <>
-                            <ScrollLinked data={account.madeProjects}>
-                            </ScrollLinked>
+                            <ScrollLinkedProjects data={account.madeProjects.filter((project: { scrappedStatus: boolean; }) => project.scrappedStatus === false)}>
+                            </ScrollLinkedProjects>
                         </>) : (<>No projects found</>)}
                 </div>
             </Card>
             <Card>
                 <div>
                     <h4>Finished projects</h4>
+                    {/* {account.madeProjects && account.madeProjects.length > 0 ? (
+                        <>
+                            <ScrollLinked data={account.madeProjects.filter(project => project.scrappedStatus === false)}>
+                            </ScrollLinked>
+                        </>) : (<>No projects found</>)} */}
                     {/* {map Account.projects if status==finished} */}
                     {/* acount.projects.name + account.projects.description account.projects.progress */}
                 </div>
@@ -93,6 +79,11 @@ const ProjectOverview = () => {
             <Card>
                 <div>
                     <h4>Scrapped projects</h4>
+                                        {account.madeProjects && account.madeProjects.length > 0 ? (
+                        <>
+                            <ScrollLinkedProjects data={account.madeProjects.filter((project: { scrappedStatus: boolean; }) => project.scrappedStatus === true)}>
+                            </ScrollLinkedProjects>
+                        </>) : (<>No projects found</>)}
                     {/* {map Account.projects if status==scrapped} */}
                     {/* acount.projects.name + account.projects.description account.projects.progress */}
                 </div>
