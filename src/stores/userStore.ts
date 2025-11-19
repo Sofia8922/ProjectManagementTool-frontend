f20302b3751acdf8a71ead76a835f3bde7529e55
Siem Nijskens <siemnijskens@gmail.com>
2025-11-19 10:18:37 +0100

    updated user store to default to NaN for id and updated project overview and project detail to redirect to login when logged out due to refreshing

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,7 +3,7 @@ import type { AccountLoginReturnDTO } from "../types/Account";
 
 // De momentele gebruiker, lekker hoog in App zodat ie overal berijkbaar is
 export const [useUser, updateUser] = createStore<AccountLoginReturnDTO>({
-    id: 0,
+    id: NaN,
     name: "",
 })
 
