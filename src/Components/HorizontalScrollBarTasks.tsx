46bb26e65f6f1bceec86f1d2b19c0de5bcab0d2d
Arne <arneemil@xs4all.nl>
2025-11-19 15:10:10 +0100

    fixed any typings

--- src/Components/HorizontalScrollBarTasks.tsx
+++ src/Components/HorizontalScrollBarTasks.tsx
@@ -9,8 +9,13 @@ import {
 import { useRef } from "react"
 import { Card } from "react-bootstrap"
 import TaskDetailModal from "./TaskDetailModal"
+import type { TaskShortDTO } from "../types/Task"
 
-export default function ScrollLinkedTasks({ data }) {
+interface ScrollLinkedTasksProps {
+    data: TaskShortDTO[]
+}
+
+export default function ScrollLinkedTasks({ data }: ScrollLinkedTasksProps) {
     const ref = useRef(null)
     const { scrollXProgress } = useScroll({ container: ref })
     const maskImage = useScrollOverflowMask(scrollXProgress)
